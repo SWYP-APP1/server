ba7551943804a448c917968b6238d840ac45a760
PodonosKim <jinsung1048@gmail.com>
2025-04-20 16:20:38 +0900

    [Fix] Update response of getting my information

--- src/main/java/com/swyp/futsal/api/user/dto/UserInfo.java
+++ src/main/java/com/swyp/futsal/api/user/dto/UserInfo.java
@@ -11,18 +11,22 @@ import java.util.Optional;
 public class UserInfo {
   private final String email;
   private final String name;
+  private final Optional<String> birthDate;
   private final Gender gender;
   private final Integer squadNumber;
   private final boolean notification;
+  private final boolean agreement;
   private final Optional<String> profileUrl;
   private final LocalDateTime createdTime;
 
   public UserInfo(User user, Optional<String> profileUrl) {
     this.email = user.getEmail();
     this.name = user.getName();
+    this.birthDate = Optional.ofNullable(user.getBirthDate());
     this.gender = user.getGender();
     this.squadNumber = user.getSquadNumber();
     this.notification = user.isNotification();
+    this.agreement = user.isAgreement();
     this.profileUrl = profileUrl;
     this.createdTime = user.getCreatedTime();
   }
